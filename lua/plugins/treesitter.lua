9a781bb5f33a363ca8843da61a28acd3954c9ce8
carlosejimenezh95 <carlosejimenezh95@gmail.com>
2025-07-01 21:28:18 -0600

    Agregar plugins para manejar el scroll y para ver el contexto, quitar archivo innecesario

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,29 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = { "css", "javascript", "json", "html", "php", "sql", "typescript" },
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end,
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				ensure_installed = { "css", "javascript", "json", "html", "php", "sql", "typescript" },
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("treesitter-context").setup({
+				enable = true, -- activado por defecto
+				max_lines = 3, -- máximo de líneas del contexto sticky
+				multiline_threshold = 20, -- mínimo de líneas del bloque para activarse
+				trim_scope = "outer", -- cómo recortar contexto si excede el max_lines
+				mode = "cursor", -- usa la posición del cursor, no del scroll
+				separator = nil, -- puedes usar un separador visual: "---"
+			})
+		end,
+	},
 }
