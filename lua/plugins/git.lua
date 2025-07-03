13f99e0b5f179f72526376972a9e120256909b19
carlosejimenezh95 <carlosejimenezh95@gmail.com>
2025-07-03 10:14:08 -0600

    Corregir error en gitsigns

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,9 +2,16 @@ return {
   {
     "tpope/vim-fugitive",
   },
-  'lewis6991/gitsigns.nvim',
-  config = function ()
-    require('gitsigns').setup()
-    vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>', {desc = 'ver los cambios previos de git'})
-  end
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+      vim.keymap.set(
+        "n",
+        "<leader>gp",
+        ":Gitsigns preview_hunk_inline<CR>",
+        { desc = "ver los cambios previos de git" }
+      )
+    end,
+  },
 }
