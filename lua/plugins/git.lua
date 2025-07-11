e9ccd70f059faafc3f5ed7c1854a3c8df88f3616
carlosejimenezh95 <carlosejimenezh95@gmail.com>
2025-07-10 21:23:31 -0600

    -Agregar keymaps para manejar cambios de git

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -6,12 +6,9 @@ return {
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup()
-      vim.keymap.set(
-        "n",
-        "<leader>gp",
-        ":Gitsigns preview_hunk_inline<CR>",
-        { desc = "ver los cambios previos de git" }
-      )
+      vim.keymap.set("n","<leader>gp",":Gitsigns preview_hunk_inline<CR>",{ desc = "ver los cambios previos de git" })
+      vim.keymap.set("n", "[c", ":Gitsigns prev_hunk<CR>", { desc = "Ir al anterior cambio"})
+      vim.keymap.set("n", "]c", ":Gitsigns next_hunk<CR>", { desc = "Ir al siguiente cambio"})
     end,
   },
 }
