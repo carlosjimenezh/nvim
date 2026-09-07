8cadc48247bb42076eba095b601f3959b708c9f3
carlosejimenezh95 <carlosejimenezh95@gmail.com>
2026-09-06 19:50:26 -0600

    cerrar neo tree si este esta activo cuando cierro el buffer actual

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -18,7 +18,21 @@ vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Cerrar" })
 vim.keymap.set("n", "<leader>e", ":lua vim.diagnostic.setqflist()<CR>", { desc = "Mostrar errores" })
 vim.keymap.set("n", "<leader>gg", ":lua require('gitsigns').setqflist()<CR>", { desc = "Mostrar cambios git" })
 vim.keymap.set("i", "jj", "<ESC>", { desc = "Salir de insertar" })
-vim.keymap.set("n", "<leader>x", ":bd<CR>", { desc = "quitar el buffer actual" })
+-- vim.keymap.set("n", "<leader>x", ":bd<CR>", { desc = "quitar el buffer actual" })
+vim.keymap.set("n", "<leader>x", function()
+  -- Verificar si Neo-tree está abierto
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    local buf = vim.api.nvim_win_get_buf(win)
+    if vim.bo[buf].filetype == "neo-tree" then
+      vim.cmd("Neotree close")
+      break
+    end
+  end
+  -- Cerrar el buffer actual
+  vim.cmd("bd")
+end, {
+  desc = "Cerrar Neo-tree y buffer actual"
+})
 -- hacer un comando para meverse arriba abajo rapidamenteq
 vim.keymap.set("n", "T", ":terminal<CR>", { desc = "Abrir terminal" })
 vim.api.nvim_set_keymap("t", "<Leader><ESC>", "<C-\\><C-n>", { noremap = true })
